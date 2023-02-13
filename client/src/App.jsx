ce3e0a16dfbb2eb898ac08da1305d433f1ba939d
you he <heyou88@hotmail.com>
2023-02-13 16:46:53 +0100

    sign

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import Wallet from "./Wallet";
 import Transfer from "./Transfer";
+import Sign from './Generator';
 import "./App.scss";
 import { useState } from "react";
 
@@ -7,6 +8,10 @@ function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
   const [privateKey, setPrivateKey] = useState("");
+  const [signature, setSignature] = useState("")
+  const [sendAmount, setSendAmount] = useState("");
+  const [recipient, setRecipient] = useState("");
+  const [nounce, setNounce] = useState ("")
   return (
     <div className="app">
       <Wallet
@@ -17,7 +22,25 @@ function App() {
         privateKey = {privateKey}
         setPrivateKey = {setPrivateKey}
       />
-      <Transfer setBalance={setBalance} address={address} />
+      <Transfer setBalance={setBalance}
+                address={address}
+                sendAmount={sendAmount}
+                setSendAmount={setSendAmount}
+                recipient={recipient}
+                setRecipient={setRecipient}
+                 />
+      <Sign
+         privateKey={privateKey}
+         setPrivateKey = {setPrivateKey}
+         signature = {signature}
+         setSignature = {setSignature}
+         sendAmount={sendAmount}
+         setSendAmount={setSendAmount}
+         recipient={recipient}
+         setRecipient={setRecipient}
+         nounce ={nounce}
+         setNounce = {setNounce}/>
+
     </div>
   );
 }
---
+++ client/src/Generator.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import server from "./server";
+import * as secp from 'ethereum-cryptography/secp256k1'
+import {toHex, utf8ToBytes} from 'ethereum-cryptography/utils'
+  function hashMessage (message){
+    return keccak256(utf8ToBytes(message))
+  }
+export default function Sign ({address,  recipient,setRecipient,sendAmount, setSendAmount}){
+  const setValue = (setter) => (evt) => setter(evt.target.value)
+  const [privateKey, setPrivateKey] = useState("");
+  const [signature, setSignature] = useState("")
+  const [nounce, setNounce] = useState ("")
+  function message(){
+const message = {
+  privateKey,
+  recipient,
+  sendAmount
+}
+return hashMessage(message)
+}
+
+async function getSig(){
+  if(privateKey && recipient && sendAmount){
+    const sig =await secp.sign(message(), privateKey,{recovered: true})
+    setSignature(sig)
+    console.log(sig)
+  }
+}
+
+return (
+  <form className="container transfer" onSubmit={getSig}>
+  <div className="container wallet">
+    <h1>Signature Generator</h1>
+
+    <label>
+      private key
+      <input placeholder="Type an private key" value={privateKey} onChange = {setValue(setPrivateKey)} ></input>
+      {/* Address: {address.slice(0,10)}... */}
+    </label>
+
+    <label>
+        Recipient
+        <input
+          placeholder="Type an address, for example: 0x2"
+          value={recipient}
+          onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+
+      <label>
+        Send Amount
+        <input
+          placeholder="1, 2, 3..."
+          value={sendAmount}
+          onChange={setValue(setSendAmount)}
+        ></input>
+      </label>
+
+    <div className="balance">signature: {signature}</div>
+    <input type="submit" className="button" value="Generate" />
+  </div>
+  </form>
+);
+}
